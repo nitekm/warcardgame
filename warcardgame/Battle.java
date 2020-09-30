1445da06beef19342601db31c3dfde531465efa8
Maciej Nitek <72076364+nitekm@users.noreply.github.com>
2020-09-30 17:14:55 +0200

    commit

--- warcardgame/Battle.java
+++ warcardgame/Battle.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public interface Battle {
     //ArrayList<Card> cardDeck1 = new ArrayList<>();
     //ArrayList<Card> cardDeck2 = new ArrayList<>();
-    
+
+    //some changes
+    //and more cahnges
     public static int battle(ArrayList<Card> cardDeck1, ArrayList<Card> cardDeck2) {return 0;};
 }
